43d82fcd1584d34ddb86671344dcb96be617a71a
Dxvah <idayraperezdelgado@gmail.com>
2023-10-30 16:49:49 +0000

    16-48 30/10
    - añadidos elementos de la escena
    - falta animar personaje
    - falta script que cambie de escena y LeanTween

---
+++ PracticaUI/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject texto;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
