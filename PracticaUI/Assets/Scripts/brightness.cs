d6c05f8e9bc9debcacc154400e71b4b6c3316527
Dxvah <idayraperezdelgado@gmail.com>
2023-10-31 16:22:32 +0000

    16-22 31/10
    -añadido slider volumen funcional
    - falta modificar scrollbar brillo

---
+++ PracticaUI/Assets/Scripts/brightness.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class brightness : MonoBehaviour
+{
+    public Scrollbar brillo;
+    public float brilloValue;
+    public Image panelBrillo;
+    void Start()
+    {
+        brillo.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, brillo.value);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void ChangeScrollbar(float value)
+    {
+        brillo.value = value;
+        PlayerPrefs.SetFloat("brillo", brilloValue);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, brilloValue);
+    }
+}
