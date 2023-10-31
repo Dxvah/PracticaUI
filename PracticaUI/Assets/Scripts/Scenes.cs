04cc3ee0b7083bc5d2ca7e3dc8de8d53bb10a3ae
Dxvah <idayraperezdelgado@gmail.com>
2023-10-31 12:22:14 +0000

    .

---
+++ PracticaUI/Assets/Scripts/Scenes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scenes: MonoBehaviour
+
+{
+   //public GameObject texto;
+    //public GameObject mainmenu;
+    //void Start()
+    //{
+//        LeanTween.moveY(texto.gameObject, 0.0f, 1.0f).setEase(LeanTweenType.easeOutBounce).setOnComplete(LoadSiguienteEscena);
+   // }
+
+//    public void LoadSiguienteEscena()
+  //  {
+        
+    //     SceneManager.LoadScene(mainmenu);
+    //}
+
+}
