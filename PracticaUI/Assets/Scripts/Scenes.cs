5c1c124b3f6cd96b5839304b44b93186bdaf0786
Dxvah <idayraperezdelgado@gmail.com>
2023-10-31 15:26:26 +0000

    15-26 31/10
    -añadido sonido
    - mejorado script para cambio de escena

--- PracticaUI/Assets/Scripts/Scenes.cs
+++ PracticaUI/Assets/Scripts/Scenes.cs
@@ -7,17 +7,19 @@ using UnityEngine.SceneManagement;
 public class Scenes: MonoBehaviour
 
 {
-   //public GameObject texto;
-    //public GameObject mainmenu;
-    //void Start()
-    //{
-//        LeanTween.moveY(texto.gameObject, 0.0f, 1.0f).setEase(LeanTweenType.easeOutBounce).setOnComplete(LoadSiguienteEscena);
-   // }
+  public GameObject logo;
+  public GameObject texto;
+  public int mainmenu;
+   void Start()
+    {
+        LeanTween.moveY(texto.gameObject, 0.0f, 1.0f).setEase(LeanTweenType.easeOutBounce).setOnComplete(LoadSiguienteEscena);
+        LeanTween.moveY(logo.gameObject, 0.0f, 2.0f).setEase(LeanTweenType.easeInBounce);
+    }
 
-//    public void LoadSiguienteEscena()
-  //  {
+   public void LoadSiguienteEscena()
+   {
         
-    //     SceneManager.LoadScene(mainmenu);
-    //}
+        SceneManager.LoadScene(mainmenu);
+   }
 
 }
